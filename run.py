0afb05d1d1e844fd0328421f455b657bf35db442
Bryce Becker <bryce.becker15@gmail.com>
2021-04-23 23:05:25 -0400

    Working on posts/replies database/routes
    Posts appears to work, replies relationship/routing with jinja needs fixed

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from stack_underflow import app
+from stack_underflow import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ stack_underflow/posts/routes.py
@@ -0,0 +1,76 @@
+from flask import (render_template, url_for, flash,
+                   redirect, request, abort, Blueprint)
+from flask_login import current_user, login_required
+from stack_underflow import db
+from stack_underflow.models import Post, Reply
+from stack_underflow.posts.forms import PostForm, ReplyForm
+
+posts = Blueprint('posts', __name__)
+
+
+@posts.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
+
+
+@posts.route("/post/<int:post_id>")
+def post(post_id):
+    form = ReplyForm()
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+# Add reply route
+@posts.route("/post/<int:post_id>/reply", methods=['GET', 'POST'])
+# @login_required
+def new_reply(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = ReplyForm()
+    if form.validate_on_submit():
+        reply = Reply(content=form.content.data, author=current_user)
+        db.session.add(reply)
+        db.session.commit()
+        flash('Your reply has been posted!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    return render_template('post.html', title='Reply Post',
+                           form=form, legend='Reply Post')
+
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
