f15a0c62e9217b97bdee0c3271cbe6f957d2e8a1
Bryce Becker <bryce.becker15@gmail.com>
2021-09-15 18:49:50 -0400

    Updated rate limiting

--- helpme_world/__init__.py
+++ helpme_world/__init__.py
@@ -14,7 +14,8 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address,
+  default_limits=["200/day", "50/hour"])
 
 
 def create_app(config_class=ProductionConfig):
